6c150cd3c29627f818a6d8765dec01c5213c4936
pettyc <pettyc@btconnect.com>
2024-05-29 15:37:10 +0100

    Update Consumer.java

--- queue-manager-basic-deployment/code/qmdemo-consumer/src/main/java/com/ibm/integration/qmdemo/Consumer.java
+++ queue-manager-basic-deployment/code/qmdemo-consumer/src/main/java/com/ibm/integration/qmdemo/Consumer.java
@@ -29,7 +29,7 @@ public class Consumer {
                         System.out.println(cf.toString());
 
                         System.out.println("Starting Consumer - creating connection...");
-                        con = cf.createConnection("app","letmein");
+                        con = cf.createConnection("app","bmV3cGFzc3dvcmQ=");
                         System.out.println("Creating session...");
                         Session session = con.createSession(false,Session.AUTO_ACKNOWLEDGE);
                         Destination getFrom = session.createQueue("DEV.QUEUE.1");
